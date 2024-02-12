c6b92009f0b9307054f2f30ad9874eec00fbef1b
Osmair Coelho <osmair.coelho@gmail.com>
2024-02-12 15:32:49 +0000

    hey-13: should make sure that only question with status DRAFT can be edited

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -6,6 +6,10 @@ use App\Models\{Question, User};
 
 class QuestionPolicy
 {
+    public function update(User $user, Question $question): bool
+    {
+        return $question->draft;
+    }
     public function publish(User $user, Question $question): bool
     {
         return $question->createdBy()->is($user);
