4ee9fe1513b1f75baa31ff3bb63b9591041d0740
Osmair Coelho <osmair.coelho@gmail.com>
2026-09-25 11:54:19 -0300

    Add script to fix Laravel permissions

---
+++ scripts/00-laravel-permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Corrigindo permissões do Laravel..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
